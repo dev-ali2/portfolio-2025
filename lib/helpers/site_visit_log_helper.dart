fda1f736ef5b826a7f7e67a21e6c0ad0cc941ade
dev-ali2 <inbx.meharali@gmail.com>
2025-06-27 09:05:45 +0500

    Minor fixes

--- lib/helpers/site_visit_log_helper.dart
+++ lib/helpers/site_visit_log_helper.dart
@@ -1,13 +1,11 @@
 import 'dart:convert';
 import 'dart:developer';
 import 'package:appwrite/appwrite.dart';
-import 'package:flutter/material.dart';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
 
 import 'package:portfolio_2025/core/common/models/device_info_model.dart';
-import 'package:portfolio_2025/core/common/models/ip_get_model.dart';
+
 import 'package:portfolio_2025/helpers/device_info_collector_helper.dart';
 
 class SiteVisitLogHelper {
@@ -19,33 +17,31 @@ class SiteVisitLogHelper {
           DeviceInfoModel.fromJson(allDeviceData);
       log('Device data serialized');
       log('${deviceInfo.systemInfo?.memory?.totalJsHeapSize}');
-      final ipResponse = await http.get(Uri.parse('https://ipapi.co/json/'));
-      log('Got IP Response: ${ipResponse.body}');
-      final ipSerializedData = IpGetModel.fromJson(jsonDecode(ipResponse.body));
-      log('IP data serialized: ${ipSerializedData.city}');
+
+      // final http.Response? ipResponse =
+      //     await http.get(Uri.parse('http://ip-api.com/json/'));
+      // IpGetModel? ipSerializedData;
+      // if (ipResponse != null) {
+      //   log('Got IP Response: ${ipResponse.body}');
+      //   ipSerializedData = IpGetModel.fromJson(jsonDecode(ipResponse.body));
+      //   log('IP data serialized: ${ipSerializedData.city}');
+      // }
+
       final Map<String, dynamic> siteLogDataToSend = {
         'deviceInfo': deviceInfo.toJson(),
-        'ipInfo': ipSerializedData.toJson(),
+        // 'ipInfo': ipSerializedData?.toJson() ?? {'ip': 'Unknown'},
         'dateTime': DateTime.now().toIso8601String(),
       };
-
-      try {
-        final databases = Databases(client);
-        final document = await databases.createDocument(
-          databaseId: dotenv.env['Database_id'] ?? '',
-          collectionId: dotenv.env['Site_logs_collection'] ?? '',
-          documentId: ID.unique(),
-          data: {'visit': jsonEncode(siteLogDataToSend)},
-        );
-        log('Site visit logged successfully: ${document.$id}');
-      } catch (e) {
-        log('Error logging site visit: $e');
-      }
+      final databases = Databases(client);
+      final document = await databases.createDocument(
+        databaseId: dotenv.env['Database_id'] ?? '',
+        collectionId: dotenv.env['Site_logs_collection'] ?? '',
+        documentId: ID.unique(),
+        data: {'visit': jsonEncode(siteLogDataToSend)},
+      );
+      log('Site visit logged successfully: ${document.$id}');
     } catch (e) {
-      Get.snackbar('Error', 'In Getting device info : $e',
-          duration: const Duration(seconds: 60),
-          backgroundColor: Colors.blue,
-          colorText: Colors.white);
+      print('Warning: $e');
     }
   }
 }
